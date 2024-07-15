c6a3d8b2fc13e0185bd5c856b54dc0dd595cfb12
Justin Tracy Manzano <justin.manzano@amusedgroup.com>
2024-07-15 21:22:12 +0800

    add read all users

--- app/routes/users.py
+++ app/routes/users.py
@@ -6,11 +6,6 @@ from typing import List
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/", response_model=List[schemas.User])
-def list_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = db.query(models.User).offset(skip).limit(limit).all()
-    return users
-
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.username == user.username).first()
@@ -38,4 +33,10 @@ def update_balance(user_id: int, balance: float, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@router.get("/", response_model=List[schemas.User])
+def list_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
+
 
